bf80cc35579c8ff46835971b13dcc39718a3c98e
Kit Heckman <kit.heckman@gmail.com>
2019-09-16 21:12:38 -0400

    oh yeah this is happening!

---
+++ JAPAN BREAK INDUSTRIES GAIDEN/Assets/animationScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationScript : MonoBehaviour
+{
+    public charControl myCharScript;
+    public Animator myAnim;
+    /*
+    public AnimationClip idle;
+    public AnimationClip run;
+    public AnimationClip squat;
+    public AnimationClip jump;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myCharScript.squat == true)
+        {
+            myAnim.Play("squat");
+        }
+        else if(myCharScript.jumping == true)
+        {
+            myAnim.Play("jump");
+        }
+        else if(Mathf.Abs(myCharScript.speed) > 0)
+        {
+            myAnim.Play("running");
+        }
+        else
+        {
+            myAnim.Play("idle");
+        }
+    }
+}
--- JAPAN BREAK INDUSTRIES GAIDEN/Assets/charControl.cs
+++ JAPAN BREAK INDUSTRIES GAIDEN/Assets/charControl.cs
@@ -7,11 +7,24 @@ public class charControl : MonoBehaviour
     public GameObject protag;
     public float speed;
     public float maxSpeed;
+    public float groundAccel;
     public float accel;
     public float dashMax;
     public float decel;
     public float dashDecel;
     public float jogMax;
+    public bool jumping;
+    public bool grounded;
+    public bool squat;
+    public int squatFrames;
+    private int squatCounter;
+    public float jumpAccel;
+    public float jumpSpeed;
+    public float gravityAccel;
+    public float termVel;
+    public float airAccel;
+    //public GameObject groundBox;
+    public Collider2D groundBox;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,10 +37,52 @@ public class charControl : MonoBehaviour
         jogMax = .05f;
         */
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D groundBox)
     {
+        //print("ground");
+        grounded = true;
+        jumping = false;
+        accel = groundAccel;
+    }
+    void jump(bool shortHop)
+    {
+        if (shortHop)
+            jumpSpeed = jumpAccel;
+        else
+            jumpSpeed = jumpAccel*1.5f;
+    }
+    void OnCollisionExit2D(Collision2D groundBox)
+    {
+        grounded = false;
+        jumping = true;
+        accel = airAccel;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (squat)
+        {
+            squatCounter += 1;
+            if(squatCounter > squatFrames)
+            {
+                squat = false;
+                jumping = true;
+                grounded = false;
+                if (Input.GetAxisRaw("Jump") > 0)
+                {
+                    jump(false);
+                }
+                else
+                {
+                    jump(true);
+                }
+            }
+            return;
+        }
+        else
+        {
+            squatCounter = 0;
+        }
         if (Input.GetAxisRaw("Horizontal") > 0)// && speed < maxSpeed)
         {
             speed += accel;
@@ -46,8 +101,15 @@ public class charControl : MonoBehaviour
         }
         else
         {
-            if (speed > 0) speed -= decel;
-            else if (speed < 0) speed += decel;
+            if (grounded == true && squat == false)
+            {
+                if (Mathf.Abs(speed) < decel)
+                {
+                    speed = 0;
+                }
+                if (speed > 0) speed -= decel;
+                else if (speed < 0) speed += decel;
+            }
         }
         if(speed == 0)
         {
@@ -74,7 +136,33 @@ public class charControl : MonoBehaviour
             speed += .01f;
         }
         */
-        print(speed);
-        protag.transform.position = new Vector3(protag.transform.position.x + speed, protag.transform.position.y, protag.transform.position.z);
+        //print(speed);
+
+        if (jumping)
+        {
+            protag.transform.position = new Vector3(protag.transform.position.x + (speed), protag.transform.position.y + jumpSpeed, protag.transform.position.z);
+            jumpSpeed -= gravityAccel;
+            if(jumpSpeed < termVel)
+            {
+                jumpSpeed = termVel;
+            }
+        }
+        else
+        {
+            protag.transform.position = new Vector3(protag.transform.position.x + speed, protag.transform.position.y, protag.transform.position.z);
+            if (speed < 0)
+            {
+                transform.localScale = new Vector3(-1.5f, 1.5f, 1);
+
+            }
+            else if (speed > 0)
+            {
+                transform.localScale = new Vector3(1.5f, 1.5f, 1);
+            }
+        }
+        if (Input.GetAxisRaw("Jump") > 0 && grounded == true && squat == false)
+        {
+            squat = true;
+        }
     }
 }
